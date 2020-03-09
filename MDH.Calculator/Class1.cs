9e760f27b93f6e6be5706d52f3c6d138646692f0
williamnordberg <william.nordberg3@gmail.com>
2020-03-09 09:39:35 +0100

    willEdit2

--- MDH.Calculator/Class1.cs
+++ MDH.Calculator/Class1.cs
@@ -18,7 +18,7 @@ namespace MDH.Calculator
         {
         double positive = double.PositiveInfinity;
         double negative = double.NegativeInfinity;
-            if (Convert.ToDouble(result)== positive|| Convert.ToDouble(result) == negative)
+            if (Convert.ToDouble(calculateResult) == positive|| Convert.ToDouble(calculateResult) == negative)
                 {
                     Console.WriteLine("divided by zero is not possible");
                 return;
