83a5d1a9467fd44c35e5ab5d7a0d4e3d8501a211
abu19002 <abu19002@LEC-CZC6229XRS>
2020-03-09 13:58:20 +0100

    Can not divide by 0

--- MDH.Calculator.Tests/UnitTest1.cs
+++ MDH.Calculator.Tests/UnitTest1.cs
@@ -7,12 +7,57 @@ namespace MDH.Calculator.Tests
     public class CalculatorTests
     {
         [TestMethod]
-        public void BasicPlus()
+        public void BasicNaturalPositives1()
         {
-            var res = Calculator.Calculate("2*2 + 1 / 3 * 2");
+            var res = Calculator.Calculate("5+6");
 
-            Assert.AreEqual(res, 4.66, 0.01);
+            Assert.AreEqual(res, 11, 0.01);
             //test push
         }
+
+        [TestMethod]
+        public void BasicNaturalPositives2()
+        {
+            var res = Calculator.Calculate("6-5");
+
+            Assert.AreEqual(res, 1, 0.01);
+            //test push
+        }
+
+        [TestMethod]
+        public void BasicNaturalPositives3()
+        {
+            var res = Calculator.Calculate("5-6");
+
+            Assert.AreEqual(res, -1, 0.01);
+            //test push
+        }
+
+        [TestMethod]
+        public void BasicNaturalPositives4()
+        {
+            var res = Calculator.Calculate("5/6");
+
+            Assert.AreEqual(res, 1, 0.01);
+            //test push
+        }
+
+        [TestMethod]
+        public void BasicNaturalPositives5()
+        {
+            var res = Calculator.Calculate("0/4");
+
+            Assert.AreEqual(res, 0, 0.01);
+            //test push
+        }
+
+        [TestMethod]
+        public void BasicNaturalPositives6()
+        {
+            var res = Calculator.Calculate("4/0");
+
+            Assert.AreEqual(res, 'can not divide by 0', 0.01);
+            //test push
+        } 
     }
 }
