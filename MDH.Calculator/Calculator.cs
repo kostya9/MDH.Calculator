e6aad0d5c2a3fb56348188b1bfe00dd4f4334334
ksi19002 <ksi19002@LEC-CZC92484L1>
2020-03-09 16:12:50 +0100

    Allo wFE integration

---
+++ MDH.Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDH.Calculator
+{
+    public static class Calculator
+    {
+        public static double Eval(string input)
+        {
+            var tree = Parser.Parse(input);
+            return 0;
+        }
+    }
+}
