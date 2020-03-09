1694c8fcb369252676669ee1e40ecf6714e731df
abu19002 <abu19002@LEC-CZC6229XRS>
2020-03-09 15:13:34 +0100

    Done

--- MDH.Calculator.Tests/UnitTest1.cs
+++ MDH.Calculator.Tests/UnitTest1.cs
@@ -6,119 +6,190 @@ namespace MDH.Calculator.Tests
     [TestClass]
     public class CalculatorTests
     {
-
-
-        
-
         [TestMethod]
-        public void Test29()
+        public void Test1()
         {
-            var res = Calculator.Calculate("6/(-a)");
-           
-            Assert.AreEqual(res, "error", 0.01);
+            var res = Calculator.Calculate("5+6");
+
+            Assert.AreEqual(res, 11, 0.01);
             
         }
 
         [TestMethod]
-        public void Test30()
+        public void Test2()
         {
-            var res = Calculator.Calculate("a+0.5");
+            var res = Calculator.Calculate("6-5");
+
+            Assert.AreEqual(res, 1, 0.01);
+            
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var res = Calculator.Calculate("5-6");
+
+            Assert.AreEqual(res, -1, 0.01);
+            
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var res = Calculator.Calculate("5/6");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test31()
+        [TestMethod]
+        public void Test5()
         {
-            var res = Calculator.Calculate("a-0.6");
+            var res = Calculator.Calculate("0/4");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test32()
+        [TestMethod]
+        public void Test6()
         {
-            var res = Calculator.Calculate("a*0.5");
+            var res = Calculator.Calculate("4/0");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test33()
+        [TestMethod]
+        public void Test7()
         {
-            var res = Calculator.Calculate("a/0.5");
+            var res = Calculator.Calculate("5*6");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test34()
+        [TestMethod]
+        public void Test8()
         {
-            var res = Calculator.Calculate("0.5-a");
+            var res = Calculator.Calculate("0*5");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test35()
+        [TestMethod]
+        public void Test9()
         {
-            var res = Calculator.Calculate("0.5/a");
+            var res = Calculator.Calculate("-5-6");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test36()
+        [TestMethod]
+        public void Test10()
         {
-            var res = Calculator.Calculate("-0.5-a");
+            var res = Calculator.Calculate("(-5)*(-5)");
 
             Assert.AreEqual(res, "error", 0.01);
             //test push
         }
 
-        public void Test37()
+        [TestMethod]
+        public void Test11()
         {
-            var res = Calculator.Calculate("0.5/a");
+            var res = Calculator.Calculate("8/(-4)");
 
-            Assert.AreEqual(res, "error", 0.01);
-            //test push
+            Assert.AreEqual(res, -2, 0.01);
+           
         }
 
-        public void Test38()
+        [TestMethod]
+        public void Test12()
         {
-            var res = Calculator.Calculate("5+6+8");
+            var res = Calculator.Calculate("(-8)/(-4)");
 
-            Assert.AreEqual(res, 19, 0.01);
-            //test push
+            Assert.AreEqual(res, 2, 0.01);
+        
         }
 
-        public void Test39()
+        [TestMethod]
+        public void Test13()
         {
-            var res = Calculator.Calculate("5-6-8");
+            var res = Calculator.Calculate("0.5+0.6");
 
-            Assert.AreEqual(res, -9, 0.01);
-            //test push
+            Assert.AreEqual(res, 1.1, 0.01);
+   
         }
 
-        public void Test40()
+        [TestMethod]
+        public void Test14()
         {
-            var res = Calculator.Calculate("5/6/8");
+            var res = Calculator.Calculate("0.5+(-0.5)");
 
-            Assert.AreEqual(res, 0.1, 0.01);
-            //test push
+            Assert.AreEqual(res, 0, 0.01);
+       
         }
 
-        public void Test41()
+        [TestMethod]
+        public void Test15()
         {
-            var res = Calculator.Calculate("5*6*8");
+            var res = Calculator.Calculate("0.5*0.6");
 
-            Assert.AreEqual(res, 240, 0.01);
-            //test push
+            Assert.AreEqual(res, 0.3, 0.01);
+       
         }
 
-        public void Test42()
+        [TestMethod]
+        public void Test16()
         {
-            var res = Calculator.Calculate("5*6-1");
+            var res = Calculator.Calculate("(-0.5)*(-0.6)");
+
+            Assert.AreEqual(res, 0, 0.01);
+            
+        }
+
+        [TestMethod]
+        public void Test17()
+        {
+            var res = Calculator.Calculate("(-0.5)*0");
+
+            Assert.AreEqual(res, 0, 0.01);
+            
+        }
+
+        [TestMethod]
+        public void Test18()
+        {
+            var res = Calculator.Calculate("(-0.5)/(-0.5)");
+
+            Assert.AreEqual(res, 1 , 0.01);
+          
+        }
+
+        [TestMethod]
+        public void Test19()
+        {
+            var res = Calculator.Calculate("(-0.6)/0.6");
+
+            Assert.AreEqual(res, -1, 0.01);
+           
+        }
+
+        [TestMethod]
+        public void Test20()
+        {
+            var res = Calculator.Calculate("0/0.5");
+
+            Assert.AreEqual(res, 0, 0.01);
+          
+        }
+
+        [TestMethod]
+        public void Test21()
+        {
+            var res = Calculator.Calculate("0.5/0");
 
             Assert.AreEqual(res, 29, 0.01);
             //test push
